d8174cf02ee7757800bc67fe8dc7c83556a2af58
SEFdevelopment <ansadovn@gmail.com>
2015-11-25 19:43:21 +0200

    Custom table view cell and collection view cell.

---
+++ MemeMe/SentMemesCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  SentMemesCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Andrei Sadovnicov on 25/11/15.
+//  Copyright © 2015 Andrei Sadovnicov. All rights reserved.
+//
+
+import UIKit
+
+class SentMemesCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - PROPERTIES
+    
+    // MARK: - @IBOutlets
+    @IBOutlet weak var topTextLabelInImage: UILabel!
+    @IBOutlet weak var bottomTextLabelInImage: UILabel!
+    
+    @IBOutlet weak var memeImageView: UIImageView!
+    
+    
+}
---
+++ MemeMe/SentMemesTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  SentMemesTableViewCell.swift
+//  MemeMe
+//
+//  Created by Andrei Sadovnicov on 25/11/15.
+//  Copyright © 2015 Andrei Sadovnicov. All rights reserved.
+//
+
+import UIKit
+
+class SentMemesTableViewCell: UITableViewCell {
+    
+    // MARK: - PROPERTIES
+    
+    // MARK: - @IBOutlets
+    @IBOutlet weak var topTextLabelInImage: UILabel!
+    @IBOutlet weak var bottomTextLabelInImage: UILabel!
+    @IBOutlet weak var topTextLabelInRow: UILabel!
+    @IBOutlet weak var bottomTextLabelInRow: UILabel!
+    
+    @IBOutlet weak var memeImageView: UIImageView!
+    
+    
+    
+}
